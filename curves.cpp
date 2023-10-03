c9d0c902dbf7e343f074d5f96dad341934ae0b59
C <thecmann1@gmail.com>
2023-10-03 17:54:33 +0200

    Add some test editing functionality

--- curves.cpp
+++ curves.cpp
@@ -37,7 +37,9 @@ class script
 		@c = create_canvas(false, 22, 22);
 		mouse.use_input(input);
 		
-		curve.type = QuadraticBezier;
+		debug_draw.line_alt_clr = 0xff555555;
+		
+		curve.type = BSpline;
 		calc_spline();
 		
 		@cam = get_active_camera();
@@ -68,27 +70,31 @@ class script
 		}
 		if(input.key_check_pressed_vk(VK::OemComma))
 		{
-			switch(curve.type)
-			{
-				case Linear: curve.type = CatmullRom; break;
-				case CatmullRom: curve.type = QuadraticBezier; break;
-				case QuadraticBezier: curve.type = CubicBezier; break;
-				case CubicBezier: curve.type = BSpline; break;
-				case BSpline: curve.type = Linear; break;
-			}
+			curve.type = CurveType(mod(curve.type + (input.key_check_gvb(GVB::Shift) ? -1 : 1), BSpline + 1));
 		}
 		
-		if(curve.end_controls == CurveEndControl::Manual)
+		if(curve.end_controls == CurveEndControl::Manual && curve.type == CatmullRom)
 		{
 			if(mouse.left_down)
 			{
-				curve.control_point_start.x = g.mouse_x_world(0, 19);
-				curve.control_point_start.y = g.mouse_y_world(0, 19);
+				curve.control_point_start.x = mouse.x;
+				curve.control_point_start.y = mouse.y;
 			}
 			if(mouse.right_down)
 			{
-				curve.control_point_end.x = g.mouse_x_world(0, 19);
-				curve.control_point_end.y = g.mouse_y_world(0, 19);
+				curve.control_point_end.x = mouse.x;
+				curve.control_point_end.y = mouse.y;
+			}
+		}
+		else
+		{
+			if(mouse.left_down)
+			{
+				curve.vertices[0].set(mouse.x, mouse.y);
+			}
+			if(mouse.right_down)
+			{
+				curve.vertices[1].set(mouse.x, mouse.y);
 			}
 		}
 		if(mouse.middle_press)
@@ -110,12 +116,12 @@ class script
 			//	: map(sin((t * 4 + PI * 1.5 + i * 1) * 0.4), -1, 1, 0.01, 12);
 			//p.tension = map(sin((t * 4 + PI + i) * 0.5), -1, 1, 0.2, 2);
 			
-			p.quad_control_point.weight = map(sin((t * 4 + PI * 1.5 + i) * 0.4), -1, 1, 0.0001, 6);
+			//p.quad_control_point.weight = map(sin((t * 4 + PI * 1.5 + i) * 0.4), -1, 1, 0.0001, 6);
 			//p.cubic_control_point_1.weight = map(sin((t * 4 + PI * 1.5 + i * 2) * 0.4), -1, 1, 0.0001, 6);
 			//p.cubic_control_point_2.weight = map(sin((t * 4 + PI * 1.5 + i * 2 + 1) * 0.4), -1, 1, 0.0001, 6);
 		}
 		//curve.vertices[1].cubic_control_point_1.weight = map(sin((t * 8 + PI * 1.5) * 0.2), -1, 1, 0.01, 6);
-		//curve.vertices[0].quad_control_point.weight = map(sin((t * 8 + PI * 1.5) * 0.2), -1, 1, 0.01, 12);
+		//curve.vertices[1].weight = map(sin((t * 8 + PI * 1.5) * 0.2), -1, 1, 0.01, 2);
 		//curve.tension = map(sin((t * 4 + PI) * 0.5), -1, 1, 0.2, 1);
 		//curve.vertices[0].tension = map(sin((t + PI + 1.2) * 1.3), -1, 1, 0.2, 10);
 		
